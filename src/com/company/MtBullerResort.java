91bda1b435b541b7c67d82818740fd06fe2c4faa
Alex Kim <69291608+alexkim1996@users.noreply.github.com>
2020-09-15 15:55:09 +1000

    added mtbuller arraylist

--- src/com/company/MtBullerResort.java
+++ src/com/company/MtBullerResort.java
@@ -1,4 +1,22 @@
 package com.company;
+import java.io.*;
+import java.util.ArrayList;
 
 public class MtBullerResort {
+
+    public static ArrayList<Accommodation> accommodation ;
+    public static ArrayList<Customer> customer ;
+    public static ArrayList<TravelPackage> travelpackage;
+
+
+    public MtBullerResort(){
+
+        accommodation = new ArrayList<>();
+        customer = new ArrayList<>();
+        travelpackage = new ArrayList<>();
+
+
+    }
 }
+
+
